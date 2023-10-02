6116c34018df15f0800e3751be393c92861938df
rick137-dev <caiulorick137@gmail.com>
2023-10-02 12:17:28 +0200

    Created Perceptron Class

---
+++ Perceptron.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Perceptron
+{
+public:
+    int learningRate;
+    vector<int> Weights;
+
+    Perceptron()
+    {
+    }
+};
