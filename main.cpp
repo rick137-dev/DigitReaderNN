cb9fbe9662aad9cf4a78ba7621f8bad95a82f26a
rick137-dev <caiulorick137@gmail.com>
2023-10-02 18:35:34 +0200

    Added file reading function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 using namespace std;
+
+string getLine(string fileAddress)
+{
+    ifstream file;
+    file.open(fileAddress);
+    string test;
+    getline(file, test);
+    file.close();
+    return test;
+}
 
 int main()
 {
