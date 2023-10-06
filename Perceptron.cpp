df68d86a6abd213355455acedab511c928e535ba
rick137-dev <caiulorick137@gmail.com>
2023-10-06 20:52:25 +0200

    Added derivatives of the three activation functions in Perceptron class

--- Perceptron.cpp
+++ Perceptron.cpp
@@ -7,13 +7,14 @@ using namespace std;
 class Perceptron
 {
 public:
-    int learningRate;
+    double learningRate;
     vector<int> Weights;
 
     Perceptron()
     {
     }
-
+    // These are the 3 activation functions for the hidden layers of the NN
+    // The name with a D prefix represents the derivatives of these functions
     double activationReLU(double x)
     {
         if (x <= 0)
@@ -26,14 +27,36 @@ public:
         }
     }
 
+    double D_activationReLU(double x)
+    {
+        if (x <= 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
     double activationSigmoid(double x)
     {
 
         return (1 / (1 + exp(-x)));
     }
 
+    double D_activationSigmoid(double x)
+    {
+        return activationSigmoid(x) * (1 - activationSigmoid(x));
+    }
+
     double activationTanh(double x)
     {
         return ((exp(2 * x) - 1) / (exp(2 * x) + 1));
+    }
+
+    double D_activationTanh(double x)
+    {
+        return 1 - (activationTanh(x)) * (activationTanh(x));
     }
 };
