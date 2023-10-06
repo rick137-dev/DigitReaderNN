0297381ffafd4721d89118960e7ec815d24ad740
rick137-dev <caiulorick137@gmail.com>
2023-10-06 19:02:21 +0200

    Added ReLu, Sigmoid and Tanh activation functions to Perceptron Class

--- Perceptron.cpp
+++ Perceptron.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -11,5 +12,28 @@ public:
 
     Perceptron()
     {
+    }
+
+    double activationReLU(double x)
+    {
+        if (x <= 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return x;
+        }
+    }
+
+    double activationSigmoid(double x)
+    {
+
+        return (1 / (1 + exp(-x)));
+    }
+
+    double activationTanh(double x)
+    {
+        return ((exp(2 * x) - 1) / (exp(2 * x) + 1));
     }
 };
