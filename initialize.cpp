e705ab6e8c230123f881d14e4a3a3f451c89bcfa
rick137-dev <caiulorick137@gmail.com>
2023-10-15 00:32:08 +0200

    Finished Initialization file and initialized weights on csv file

--- initialize.cpp
+++ initialize.cpp
@@ -6,24 +6,56 @@
 #include <sstream>
 #include <string>
 #include <random>
+#include <limits>
+typedef std::numeric_limits<double> dbl;
 
 using namespace std;
 
-double HeInit(int numberOfInputs)
+void initializeWeights(string fileAddress = "Weights.csv")
 {
-    double standardDeviation = sqrt(2 / numberOfInputs);
+    double standardDeviation = sqrt(2 / (double)784);
     std::default_random_engine generator;
     std::normal_distribution<double> distribution(0.0, standardDeviation);
-    return distribution(generator);
-}
 
-void initializeWeights(string fileAddress, int Width, int Length)
-{
-   
+    ofstream file;
+    file.open(fileAddress);
+    int i, j;
+    for (i = 0; i <= 783; i++)
+    {
+        for (j = 0; j <= 499; j++)
+        {
+            file << distribution(generator)
+                 << ",";
+        }
+        file << "\n";
+    }
+
+    standardDeviation = sqrt(2 / (double)500);
+    std::normal_distribution<double> distribution1(0.0, standardDeviation);
+    for (i = 0; i <= 499; i++)
+    {
+        for (j = 0; j <= 99; j++)
+        {
+            file << distribution1(generator)
+                 << ",";
+        }
+        file << "\n";
+    }
+
+    for (i = 0; i <= 99; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            file << "1"
+                 << ",";
+        }
+        file << "\n";
+    }
+
+    file.close();
 }
 
 int main()
 {
-
-    cout << "This works!";
+    initializeWeights();
 }
