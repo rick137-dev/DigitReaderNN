e3a269ff633e92f2eb076d2c48beaa276e59359c
rick137-dev <caiulorick137@gmail.com>
2023-10-18 18:52:24 +0200

    Added header file to share functions defined in file

---
+++ stringProcessing.h
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "Neuron.h"
+
+using namespace std;
+
+string getSpecificLineDataset(int line)
+{
+}
+
+string getSpecificLineWeights(int line)
+{
+}
+
+string getWeightsGivenLayerNeuron(int Layer, int Neuron)
+{
+}
+
+vector<double> processWeightString(string input) {}
+
+vector<int> processInputString(string input) {}
+
+vector<double> getNormalizedInput(int line)
+{
+}
+
+vector<double> getBiases(int Layer)
+{
+}
+double getBias(int Layer, int Neuron)
+{
+}
