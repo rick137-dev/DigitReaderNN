f1bac44742419a04cf4d9a11d4cf92d35eeaccbc
rick137-dev <caiulorick137@gmail.com>
2023-10-18 15:48:29 +0200

    Added functions and completed functions for string retrieval and changing to vector form

--- stringProcessing.cpp
+++ stringProcessing.cpp
@@ -7,17 +7,16 @@
 #include "Neuron.h"
 
 using namespace std;
-string fileAddress = "MNIST/train.csv";
-double divisor = 255;
 
-string getLine(string fileAddress, int i)
+string getSpecificLineDataset(int line)
 {
-    i++;
+    string fileAddress = "MNIST/train.csv";
+    line++;
     int x = 0;
     string test;
     ifstream file;
     file.open(fileAddress);
-    while (x < i)
+    while (x < line)
     {
 
         getline(file, test);
@@ -28,12 +27,49 @@ string getLine(string fileAddress, int i)
     return test;
 }
 
-string getWeights(int Layer, int Neuron)
+string getSpecificLineWeights(int line)
 {
+    string fileAddress = "Weights.csv";
+    line++;
+    int x = 0;
+    string test;
+    ifstream file;
+    file.open(fileAddress);
+    while (x < line)
+    {
+
+        getline(file, test);
+
+        x++;
+    }
+    file.close();
+    return test;
 }
 
-int *processWeights(string weights, int (&array)[2])
+// This function allocates vector on stack and copies it to return it, must find fix for efficiency (allocate on heap and return pointer)
+vector<int> processString(string input)
 {
+    vector<int> numbers;
+
+    stringstream ss(input);
+    while (ss.good())
+    {
+        string substr;
+        getline(ss, substr, ',');
+        numbers.push_back(stoi(substr));
+    }
+    return numbers;
+}
+// same inefficiency here
+vector<double> getNormalizedWeights(int line)
+{
+    vector<int> weightsI = processString(getSpecificLineWeights(line));
+    vector<double> final;
+    for (int i = 0; i < weightsI.size(); i++)
+    {
+        final.push_back((weightsI[i]) / (double)255);
+    }
+    return final;
 }
 
 int main()
