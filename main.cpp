1f8244839452c2cc4873ca65ba2e39ed64000cc7
rick137-dev <caiulorick137@gmail.com>
2023-11-02 20:43:56 +0100

    Small edit to main

--- main.cpp
+++ main.cpp
@@ -7,8 +7,12 @@
 
 // This program utillizes 3 layers, 500-100-10, it uses the ReLu activation function and softmax at the output layer
 
+// Training is done with Stochastic Gradient Descent, with mini-batches of 100 inputs
+
 using namespace std;
 
 int main()
 {
+    NeuralNetwork n;
+    n.getNNOutput(1);
 }
